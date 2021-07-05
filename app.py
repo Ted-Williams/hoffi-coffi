7476a961199bcad29f39898b9fb398718ec3ae6b
Ted-Williams <ted_Williams@hotmail.co.uk>
2021-07-05 20:48:08 +0000

    changed the templates to be more inclusive of the base template

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 import os
-from flask import (Flask, render_template, url_for)
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask (__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
 def homepage():
+    """
+    Function which takes the user to the 
+    homepage when the app loads
+    """
     return render_template('pages/homepage.html')
+
+app.route("/login", methods=["GET", "POST"])
+def log_in():
+    """
+    This allows the user to log in to
+    the app using their username and password
+    """
 
 
 if __name__ == "__main__":
