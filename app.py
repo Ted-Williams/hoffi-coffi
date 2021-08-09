12e2fcd096192d45ca57f14eedf81bde3f81167f
Ted-Williams <ted_Williams@hotmail.co.uk>
2021-08-09 18:47:04 +0000

    added add and delete routes

--- app.py
+++ app.py
@@ -27,7 +27,12 @@ def homepage():
     Function which takes the user to the 
     homepage when the app loads
     """
-    return render_template('pages/homepage.html', homepage=homepage, loggedOut=1)
+    coffeeList = mongo.db.coffee.find({})
+    formattedCoffeeList = []
+    for coffee in list(coffeeList):
+        coffee['_id'] = str(coffee['_id'])
+        formattedCoffeeList.append(coffee)
+    return render_template('pages/homepage.html', coffeeList=list(formattedCoffeeList), loggedOut=1)
     
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -71,13 +76,17 @@ def admin(user_id):
     Shows the user that they are logged in 
     and are able to add/delete a coffee
     """
+    added = request.args.get('added')
+    if added: added = int(added)
+    deleted = request.args.get('deleted')
+    if deleted: deleted = int(deleted)
     coffeeList = mongo.db.coffee.find({})
     formattedCoffeeList = []
     for coffee in list(coffeeList):
         coffee['_id'] = str(coffee['_id'])
         formattedCoffeeList.append(coffee)
     if request.method == "GET":
-        return render_template('pages/admin.html', coffeeList=list(formattedCoffeeList), loggedIn=1)
+        return render_template('pages/admin.html', coffeeList=list(formattedCoffeeList), loggedIn=1, user_id=user_id, added=added, deleted=deleted)
     else:
         coffee_id = request.form.get('coffeeControl')
         return redirect(url_for("adminEditCoffee", user_id=user_id, coffee_id=coffee_id))
@@ -90,19 +99,69 @@ def adminEditCoffee(user_id, coffee_id):
     """
     if request.method == "GET":
         coffee = mongo.db.coffee.find_one({"_id": ObjectId(coffee_id)})
-    return render_template('pages/admin-edit.html', coffee=coffee, loggedIn=1)
+    return render_template('pages/admin-edit.html', coffee=coffee, loggedIn=1, user_id=user_id)
 
-@app.route("/edit/coffee")
-def coffee():
+@app.route("/admin/update-coffee/<user_id>/<coffee_id>", methods=["GET", "POST"])
+def adminUpdateCoffee(user_id, coffee_id):
     """
-    shows the static coffee page to all users
+    Updates the database values with user input field data
     """
-    return render_template('pages/coffee.html')
+    if request.method == "POST":
+        newValues = { "$set": {
+            "product_name": request.form.get('coffeeName'),
+            "price": request.form.get('price'),
+            "origin": request.form.get('origin'),
+            "taste": request.form.get('taste'),
+            "image_url": request.form.get('image_url'),
+        }}
+        result = mongo.db.coffee.update_one({"_id": ObjectId(coffee_id)}, newValues)
+        coffee = mongo.db.coffee.find_one({"_id": ObjectId(coffee_id)})
+        if result:
+            return render_template('pages/admin-edit.html', coffee=coffee, loggedIn=1, user_id=user_id, updated=1)
+        else:
+            return render_template('pages/admin-edit.html', coffee=coffee, loggedIn=1, user_id=user_id, updated=0)
+
+@app.route("/admin/delete-coffee/<user_id>/<coffee_id>", methods=["GET", "POST"])
+def adminDeleteCoffee(user_id, coffee_id):
+    """
+    Delete a database entry using coffee_id
+    """
+    result = mongo.db.coffee.delete_one({"_id": ObjectId(coffee_id)})
+    if result and result.deleted_count:
+        return redirect(url_for("admin", loggedIn=1, user_id=user_id, deleted=1))
+    else:
+        return redirect(url_for("admin", loggedIn=1, user_id=user_id, deleted=0))
+
+@app.route("/admin/add-coffee/<user_id>", methods=["GET", "POST"])
+def adminAddCoffee(user_id):
+    """
+    Insert a new database entry
+    """
+    if request.method == "GET":
+        return render_template('pages/admin-add.html', loggedIn=1, user_id=user_id)
+    else:
+        product_name = request.form.get('coffeeName')
+        if product_name: 
+            newValues = {
+                "product_name": product_name,
+                "price": request.form.get('price'),
+                "origin": request.form.get('origin'),
+                "taste": request.form.get('taste'),
+                "image_url": request.form.get('image_url'),
+            }
+            result = mongo.db.coffee.insert_one(newValues)
+            if result and result.inserted_id:
+                return redirect(url_for("admin", loggedIn=1, user_id=user_id, added=1))
+            else:
+                return redirect(url_for("admin", loggedIn=1, user_id=user_id, added=0))
+        else:
+            return render_template('pages/admin-add.html', loggedIn=1, user_id=user_id, missingName=1)
+
 
 @app.route("/forward")
 def move_forward():
     print("Moving Forward...")
-    return render_template('pages/homepage.html')
+    # return render_template('pages/admin-edit.html', coffee=coffee, loggedIn=1)
 
 
 if __name__ == "__main__":
