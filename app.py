5565c06143b1bc60a771e02a9d25656f3a81de28
Ted-Williams <ted_Williams@hotmail.co.uk>
2021-08-10 19:56:19 +0000

    logout added

--- app.py
+++ app.py
@@ -16,9 +16,10 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-
 mongo = PyMongo(app)
 
+loggedIn = False
+loggedInUserId = None
 
 
 @app.route("/")
@@ -32,7 +33,11 @@ def homepage():
     for coffee in list(coffeeList):
         coffee['_id'] = str(coffee['_id'])
         formattedCoffeeList.append(coffee)
-    return render_template('pages/homepage.html', coffeeList=list(formattedCoffeeList), loggedOut=1)
+
+    if loggedIn == True:
+        return render_template('pages/homepage.html', coffeeList=list(formattedCoffeeList), loggedIn=1, user_id=loggedInUserId)
+    else:
+        return render_template('pages/homepage.html', coffeeList=list(formattedCoffeeList), loggedIn=0, user_id=loggedInUserId)
     
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -40,15 +45,14 @@ def login():
     This allows the user to log in to
     the app using their username and password
     """
-    
     if request.method == "POST":
         user = mongo.db.users.find_one({'email' : request.form.get('email')})
         if user:
-            #if check_password_hash(user["password"],
-            #request.form.get("password")):
             if user['password'] == request.form.get('password'):
-                session["users"] = request.form.get("email").lower()
-                flash("Welcome, {}".format(request.form.get("email")))
+                global loggedIn
+                loggedIn = True
+                global loggedInUserId
+                loggedInUserId = user["_id"]
                 return redirect(url_for("admin", user_id=user["_id"]))
 
         else:
@@ -67,8 +71,12 @@ def logout():
     Retuns the user back to the hompage when logged
     out
     """
+    global loggedIn
+    loggedIn = None
+    global loggedInUserId
+    loggedInUserId = None
     session.clear()
-    return render_template('pages/homepage.html')
+    return redirect(url_for('homepage'))
 
 @app.route("/admin/<user_id>", methods=["GET", "POST"])
 def admin(user_id):
@@ -76,20 +84,23 @@ def admin(user_id):
     Shows the user that they are logged in 
     and are able to add/delete a coffee
     """
-    added = request.args.get('added')
-    if added: added = int(added)
-    deleted = request.args.get('deleted')
-    if deleted: deleted = int(deleted)
-    coffeeList = mongo.db.coffee.find({})
-    formattedCoffeeList = []
-    for coffee in list(coffeeList):
-        coffee['_id'] = str(coffee['_id'])
-        formattedCoffeeList.append(coffee)
-    if request.method == "GET":
-        return render_template('pages/admin.html', coffeeList=list(formattedCoffeeList), loggedIn=1, user_id=user_id, added=added, deleted=deleted)
+    if loggedIn == True:
+        added = request.args.get('added')
+        if added: added = int(added)
+        deleted = request.args.get('deleted')
+        if deleted: deleted = int(deleted)
+        coffeeList = mongo.db.coffee.find({})
+        formattedCoffeeList = []
+        for coffee in list(coffeeList):
+            coffee['_id'] = str(coffee['_id'])
+            formattedCoffeeList.append(coffee)
+        if request.method == "GET":
+            return render_template('pages/admin.html', coffeeList=list(formattedCoffeeList), loggedIn=1, user_id=user_id, added=added, deleted=deleted)
+        else:
+            coffee_id = request.form.get('coffeeControl')
+            return redirect(url_for("adminEditCoffee", user_id=user_id, coffee_id=coffee_id))
     else:
-        coffee_id = request.form.get('coffeeControl')
-        return redirect(url_for("adminEditCoffee", user_id=user_id, coffee_id=coffee_id))
+        return redirect(url_for('login'))
 
 @app.route("/admin/<user_id>/<coffee_id>", methods=["GET", "POST"])
 def adminEditCoffee(user_id, coffee_id):
@@ -97,71 +108,77 @@ def adminEditCoffee(user_id, coffee_id):
     Shows the user that they are logged in 
     and are able to add/delete a coffee
     """
-    if request.method == "GET":
-        coffee = mongo.db.coffee.find_one({"_id": ObjectId(coffee_id)})
-    return render_template('pages/admin-edit.html', coffee=coffee, loggedIn=1, user_id=user_id)
+    if loggedIn == True:
+        if request.method == "GET":
+            coffee = mongo.db.coffee.find_one({"_id": ObjectId(coffee_id)})
+        return render_template('pages/admin-edit.html', coffee=coffee, loggedIn=1, user_id=user_id)
+    else:
+        return redirect(url_for('login'))
 
 @app.route("/admin/update-coffee/<user_id>/<coffee_id>", methods=["GET", "POST"])
 def adminUpdateCoffee(user_id, coffee_id):
     """
     Updates the database values with user input field data
     """
-    if request.method == "POST":
-        newValues = { "$set": {
-            "product_name": request.form.get('coffeeName'),
-            "price": request.form.get('price'),
-            "origin": request.form.get('origin'),
-            "taste": request.form.get('taste'),
-            "image_url": request.form.get('image_url'),
-        }}
-        result = mongo.db.coffee.update_one({"_id": ObjectId(coffee_id)}, newValues)
-        coffee = mongo.db.coffee.find_one({"_id": ObjectId(coffee_id)})
-        if result:
-            return render_template('pages/admin-edit.html', coffee=coffee, loggedIn=1, user_id=user_id, updated=1)
-        else:
-            return render_template('pages/admin-edit.html', coffee=coffee, loggedIn=1, user_id=user_id, updated=0)
+    if loggedIn == True:
+        if request.method == "POST":
+            newValues = { "$set": {
+                "product_name": request.form.get('coffeeName'),
+                "price": request.form.get('price'),
+                "origin": request.form.get('origin'),
+                "taste": request.form.get('taste'),
+                "image_url": request.form.get('image_url'),
+            }}
+            result = mongo.db.coffee.update_one({"_id": ObjectId(coffee_id)}, newValues)
+            coffee = mongo.db.coffee.find_one({"_id": ObjectId(coffee_id)})
+            if result:
+                return render_template('pages/admin-edit.html', coffee=coffee, loggedIn=1, user_id=user_id, updated=1)
+            else:
+                return render_template('pages/admin-edit.html', coffee=coffee, loggedIn=1, user_id=user_id, updated=0)
+    else:
+        return redirect(url_for('login'))
 
 @app.route("/admin/delete-coffee/<user_id>/<coffee_id>", methods=["GET", "POST"])
 def adminDeleteCoffee(user_id, coffee_id):
     """
     Delete a database entry using coffee_id
     """
-    result = mongo.db.coffee.delete_one({"_id": ObjectId(coffee_id)})
-    if result and result.deleted_count:
-        return redirect(url_for("admin", loggedIn=1, user_id=user_id, deleted=1))
+    if loggedIn == True:
+        result = mongo.db.coffee.delete_one({"_id": ObjectId(coffee_id)})
+        if result and result.deleted_count:
+            return redirect(url_for("admin", loggedIn=1, user_id=user_id, deleted=1))
+        else:
+            return redirect(url_for("admin", loggedIn=1, user_id=user_id, deleted=0))
     else:
-        return redirect(url_for("admin", loggedIn=1, user_id=user_id, deleted=0))
+        return redirect(url_for('login'))
 
 @app.route("/admin/add-coffee/<user_id>", methods=["GET", "POST"])
 def adminAddCoffee(user_id):
     """
     Insert a new database entry
     """
-    if request.method == "GET":
-        return render_template('pages/admin-add.html', loggedIn=1, user_id=user_id)
-    else:
-        product_name = request.form.get('coffeeName')
-        if product_name: 
-            newValues = {
-                "product_name": product_name,
-                "price": request.form.get('price'),
-                "origin": request.form.get('origin'),
-                "taste": request.form.get('taste'),
-                "image_url": request.form.get('image_url'),
-            }
-            result = mongo.db.coffee.insert_one(newValues)
-            if result and result.inserted_id:
-                return redirect(url_for("admin", loggedIn=1, user_id=user_id, added=1))
-            else:
-                return redirect(url_for("admin", loggedIn=1, user_id=user_id, added=0))
+    if loggedIn == True:
+        if request.method == "GET":
+            return render_template('pages/admin-add.html', loggedIn=1, user_id=user_id)
         else:
-            return render_template('pages/admin-add.html', loggedIn=1, user_id=user_id, missingName=1)
-
-
-@app.route("/forward")
-def move_forward():
-    print("Moving Forward...")
-    # return render_template('pages/admin-edit.html', coffee=coffee, loggedIn=1)
+            product_name = request.form.get('coffeeName')
+            if product_name: 
+                newValues = {
+                    "product_name": product_name,
+                    "price": request.form.get('price'),
+                    "origin": request.form.get('origin'),
+                    "taste": request.form.get('taste'),
+                    "image_url": request.form.get('image_url'),
+                }
+                result = mongo.db.coffee.insert_one(newValues)
+                if result and result.inserted_id:
+                    return redirect(url_for("admin", loggedIn=1, user_id=user_id, added=1))
+                else:
+                    return redirect(url_for("admin", loggedIn=1, user_id=user_id, added=0))
+            else:
+                return render_template('pages/admin-add.html', loggedIn=1, user_id=user_id, missingName=1)
+    else:
+        return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,52 @@
 onSelectCoffee = (event) => {
-const coffee = document.getElementById('edit-coffee').value;
-const selectedCoffee = JSON.parse(JSON.stringify(coffee));
-document.getElementsByName('coffeeName').value = selectedCoffee.product_name;
-console.log(selectedCoffee)
-} 
+  const coffee = document.getElementById('edit-coffee').value;
+  const selectedCoffee = JSON.parse(JSON.stringify(coffee));
+  document.getElementsByName('coffeeName').value = selectedCoffee.product_name;
+  console.log(selectedCoffee)
+}
+
+submitContactForm = () => {
+  const successAlert = `
+  <div class="alert alert-primary alert-dismissible" role="alert" id="contactSuccessAlert">
+    Message successfully sent!
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+  `
+  const failedAlert = `
+  <div class="alert alert-danger alert-dismissible" role="alert" id="contactFailedAlert">
+    Message failed to send. Please try again later.
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+  `
+
+  const name = document.getElementById('user_name').value;
+  const email = document.getElementById('user_email').value;
+  const message = document.getElementById('message').value;
+
+  if (name && email && message) {
+    emailjs.sendForm('service_dh6n4z7', 'template_g4njnc7', '#contactForm').then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+      const contactAlert = document.getElementById('contactAlert')
+      contactAlert.innerHTML = successAlert;
+    }, (error) => {
+      console.log('FAILED...', error);
+      const contactAlert = document.getElementById('contactAlert')
+      contactAlert.innerHTML = failedAlert;
+    });
+  } else {
+    let missingFields = [];
+    if (!name) missingFields.push('Name');
+    if (!email) missingFields.push('Email');
+    if (!message) missingFields.push('Message');
+
+    const missingFieldsMessage = missingFields.join(' and ');
+    const failedAlert = `
+    <div class="alert alert-danger alert-dismissible" role="alert" id="contactFailedAlert">
+      ${missingFieldsMessage} ${missingFields.length === 1 ? 'is a required field' : 'are required fields'}.
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    `
+    const contactAlert = document.getElementById('contactAlert');
+    contactAlert.innerHTML = failedAlert;
+  }
+}
